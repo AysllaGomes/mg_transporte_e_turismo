ac9a92d5fcf335685463d9677f0a4ebaefb69d86
ayslla.gomes <04414927110@git.economia.gov.br>
2020-08-12 17:03:20 -0300

    Adicionando as imagens do docker, referente ao front

---
+++ docker/frontend/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ -d "/var/www/html/node_modules" ]
+ then
+    rm -rf node_modules
+fi
+
+if [ -d "/var/www/html/dist" ]
+then
+    echo "Apagando o dist anterior"
+    rm -rf dist
+fi
+
+mkdir "dist"
+
+export NG_CLI_ANALYTICS=false
+echo "Instalando o node"
+npm install
+
+buildEnv=${AMBIENTE:=prod}
+echo "Buildando o projeto - ambiente: $buildEnv"
+ng build --configuration=$buildEnv
+
+echo "Mudando o dono da pasta dist"
+chown -R 1000:1000 dist
+chmod -R 777 dist
